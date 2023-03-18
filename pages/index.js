e2ba9947fa4f683c7cf22de5c182a4afdc16e518
769399481 <86551711+769399481@users.noreply.github.com>
2023-03-18 16:41:15 +0700

    modify

--- pages/index.js
+++ pages/index.js
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <div className="center">
       <Head>
-        <title>Hao's Final Exam</title>
+        <title>Hao Final Exam</title>
       </Head>
-      <h1>Hao's Final Exam</h1>
+      <h1>Hao Final Exam</h1>
       <p>This is the initial page to jump to the final exam.</p>
       <Link href="/suppliers">Suppliers</Link>
     </div>
