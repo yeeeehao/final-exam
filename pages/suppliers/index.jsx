1fd3d28b2ac8e25f6d752863ceec210ef6686cda
769399481 <86551711+769399481@users.noreply.github.com>
2023-03-18 17:11:34 +0700

    change fetch

--- pages/suppliers/index.jsx
+++ pages/suppliers/index.jsx
@@ -170,7 +170,7 @@ export default function Home({ suppliers }) {
   );
 }
 export async function getServerSideProps() {
-  const res = await fetch(`http://localhost:3000/api/suppliers/information/`);
+  const res = await fetch(`https://final-exam-6238023.vercel.app/api/suppliers/information/`);
   const suppliers = await res.json();
   // console.debug('supplier 1', suppliers)
   return { props: { suppliers } };
